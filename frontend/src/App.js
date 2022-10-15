9b1f79c2e371e6d19fd2f6711d5fdd0510037b4f
Kelly Shen <kellyshenfrog@gmail.com>
2022-10-15 14:58:19 -0500

    webgazer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import WebGazer from './WebGazer';
+import Book from './Book';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Book />
     </div>
   );
 }
---
+++ frontend/src/Book.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import FlipPage from 'react-flip-page'
+
+const Book = () => {
+    return (
+        <FlipPage>
+            <article>
+                <h1>My awesome first article</h1>
+                <p>My awesome first content</p>
+            </article>
+            <article>
+                <h1>My wonderful second article</h1>
+                <p>My wonderful second content</p>
+            </article>
+            <article>
+                <h1>My excellent third article</h1>
+                <p>My excellent third content</p>
+            </article>
+        </FlipPage>
+    )
+}
+
+export default Book;
